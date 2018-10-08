07d9e93493b8a8cb17a98c0a4da5902f5e8f6b90
Thinesh <thineshprathaban@gmail.com>
2018-10-08 20:49:43 +0800

    Completed Part 3

--- app/src/main/java/com/example/foodie/foodie/Home.java
+++ app/src/main/java/com/example/foodie/foodie/Home.java
@@ -1,5 +1,6 @@
 package com.example.foodie.foodie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -36,6 +37,8 @@ public class Home extends AppCompatActivity
 
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
+
+    FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter;
 
     FloatingActionButton fab;
     @Override
@@ -98,7 +101,7 @@ public class Home extends AppCompatActivity
     }
 
     private void loadMenu(){
-        FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class, R.layout.menu_item,MenuViewHolder.class,category) {
+        adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class, R.layout.menu_item,MenuViewHolder.class,category) {
             @Override
             protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
                 viewHolder.txtMenuName.setText(model.getName());
@@ -107,7 +110,9 @@ public class Home extends AppCompatActivity
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(Home.this, ""+clickItem.getName(),Toast.LENGTH_SHORT).show();
+                       Intent foodList = new Intent(Home.this,FoodList.class);
+                       foodList.putExtra("CategoryId",adapter.getRef(position).getKey());
+                       startActivity(foodList);
                     }
                 });
             }
